aa1cd2b45f15c32d092014c558329001371aa064
php 10 <php.10@metralha.cec.ime.usp.br>
2017-02-09 21:56:54 -0200

    static

--- static/Conta.php
+++ static/Conta.php
@@ -1,20 +1,25 @@
 <?php
+// Fazer o contador contar quantas instancias de Conta terem
+//na memória;
 
 class Conta{
 
-public $numero;
-public $saldo;
-public $contador;
+public $numero; // Atributo do objeto
+public $saldo;  // Atributo do objeto
+public static $contador = 0; // Atributo da classe
+
+// STATIC - É O MESMO QUE DA CLASSE.
 
 public function __construct(){
-  $this->contador++;
+  self::$contador++; // Para os objetos terem acessos ao atributo.
 }
-{
-  $this->foo = $foo;
-}
-{
-  # code...
-}
+    public static function zeraContador(){
+
+    self::$contador = 0;
+    //Envia email;
+    //Zera o contador;
+  }
+ $numConta = new Conta
 }
 
  ?>
--- static/TestaConta.php
+++ static/TestaConta.php
@@ -1,6 +1,9 @@
 <?php
 require_once('Conta.php');
 
+echo "Contador: ".Conta::$contador . PHP_EOL;
+// .Conta::$contador é para chamar o $contador da classe Conta.php.
+
 $conta = new Conta;
 $conta->numero = "11";
 $conta->saldo = "600";
@@ -8,5 +11,13 @@ $conta->saldo = "600";
 $conta2 = new Conta;
 $conta->numero = "12";
 $conta->saldo = "130";
+
+echo "Contador final:".Conta::$contador . PHP_EOL;
+
+Conta::zeraContador();
+
+echo "Contador após zera contador: " . Conta::$contador . PHP_EOL;
+
+
 
  ?>
